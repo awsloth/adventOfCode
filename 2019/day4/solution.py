1a205826d641f25f066355de57c42295e60357f3
awsloth <awsloth@gmail.com>
2023-11-04 12:59:13 +0000

    2019-4

---
+++ 2019/day4/solution.py
@@ -0,0 +1,74 @@
+import os
+year, day = [2019, 4]
+root = f"C:\\Users\\Adam\\PythonProjects\\adventOfCode\\{year}\\day{day}"
+
+def main(enabled_print=True, test=False, debug=False):
+    if debug:
+        with open(os.path.join(root, "input.txt"), 'r') as f:
+            inp = [line.strip() for line in f.readlines()]
+    elif test:
+        with open(os.path.join(root, "test.txt"), 'r') as f:
+            inp = [line.strip() for line in f.readlines()]
+    else:
+        with open(os.path.join(root, "input.txt"), 'r') as f:
+            inp = [line.strip() for line in f.readlines()]
+
+    low, top = map(int, inp[0].split("-"))
+
+    total = 0
+    for i in range(low, top+1):
+        if "".join(map(str, sorted([int(c) for c in str(i)]))) != str(i):
+            continue
+
+        valid = False
+        for (elem1, elem2) in zip(str(i), str(i)[1:]):
+            if elem1==elem2:
+                valid = True
+            
+        if valid:
+            total += 1
+
+    return total
+
+if __name__ == "__main__":
+    from aocd import submit
+
+    import bs4
+    import copier
+    import sys
+
+    if (len(sys.argv) < 3):
+        print("Enter command line arguments")
+        exit(-1)
+    
+    arg_list = sys.argv[1:]
+    complete = arg_list[0].lower() == "true"
+    run_test = arg_list[1].lower() == "true"
+
+    if (len(sys.argv) == 4):
+        test_ans = int(sys.argv[3])
+    elif (run_test):
+        print("Provide answer to test")
+        exit(-1)
+
+    if (run_test and not os.path.exists(os.path.join(root, "test.txt"))):
+        print("Test file does not exist")
+        exit(-1)
+
+    answer = main(not complete, run_test, complete)
+    
+    if run_test:
+        print(f"The answer is {test_ans}, you got {answer}.")
+        if (test_ans == answer):
+            print(f"You got it right! Time to submit!")
+        else:
+            print("Time to change ur code :(")
+    elif complete:
+        r = submit(answer, year=year, day=day)
+        if r is not None:
+            soup = bs4.BeautifulSoup(r.data, "html.parser")
+            message = soup.article.text
+            if "That's the right answer" in message:
+                copier.make_next(year, day)
+    else:
+        print(f"You got {answer}")
