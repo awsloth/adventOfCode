73acb0f5c11fe174ba5d45e27bcff9497296115c
Anna <awsloth@gmail.com>
2024-12-07 01:27:46 +0000

    Changes to day 6

--- 2024/day6/LeanSoln/LeanSoln/soln2.lean
+++ 2024/day6/LeanSoln/LeanSoln/soln2.lean
@@ -1,16 +1,5 @@
 import LeanSoln.data
 
---def data := "....#.....
---.........#
---..........
---..#.......
---.......#..
---..........
---.#..^.....
---........#.
---#.........
---......#..."
-
 def lines (s : String) : List String := s.splitOn "\n"
 
 def grid : List String → List (List Char) := List.map (λ s ↦ s.data)
@@ -52,34 +41,21 @@ def Dir.toString : Dir → String
 instance : ToString Dir where
   toString d := Dir.toString d
 
-def findXCol : Nat → List (Nat × Nat × Char) → List (Nat × Nat × Char)
+def findXCol : Nat → List (Nat × Nat) → List (Nat × Nat)
   | x, xs => List.filter (λ y ↦ y.fst = x) xs
 
-def findYRow : Nat → List (Nat × Nat × Char) → List (Nat × Nat × Char)
-  | y, xs => List.filter (λ x ↦ x.snd.fst = y) xs
+def findYRow : Nat → List (Nat × Nat) → List (Nat × Nat)
+  | y, xs => List.filter (λ x ↦ x.snd = y) xs
 
-def findFirst : (Nat × Nat × Char → Bool) → List (Nat × Nat × Char) → Nat × Nat × Char → Nat × Nat × Char
+def findFirst : (Nat × Nat → Bool) → List (Nat × Nat) → Nat × Nat → Nat × Nat
   | _, [], dummy => dummy
   | f, (x :: xs), dummy => if f x then x else findFirst f xs dummy
 
-def findEnd : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat × Char) → Nat × Nat
-  | (_, _), x, y, Dir.Up, xs => let pend := stripLast (findFirst (λ p ↦ p.snd.fst < y) (findXCol x xs).reverse (x, 1000, '#')) ; if pend.snd = 1000 then (pend.fst, 0) else (pend.fst, pend.snd + 1)
-  | (_, maxy), x, y, Dir.Down, xs => let pend := stripLast (findFirst (λ p ↦ p.snd.fst > y) (findXCol x xs) (x, 1000, '#')) ; if pend.snd = 1000 then (pend.fst, maxy-1) else (pend.fst, pend.snd - 1)
-  | (_, _), x, y, Dir.Left, xs => let pend := stripLast (findFirst (λ p ↦ p.fst < x) (findYRow y xs).reverse (1000, y, '#')); if pend.fst = 1000 then (0, pend.snd) else (pend.fst + 1, pend.snd)
-  | (maxx, _), x, y, Dir.Right, xs => let pend := stripLast (findFirst (λ p ↦ p.fst > x) (findYRow y xs) (1000, y, '#')) ; if pend.fst = 1000 then (maxx-1, pend.snd) else (pend.fst - 1, pend.snd)
-
-def rangeBetween : Nat → Nat → List Nat
-  | x, y => List.map (.+x) (List.range (y - x + 1))
-
-def repeatN {α : Type} : α → Nat → List α
-  | _, 0 => []
-  | a, Nat.succ n => a :: (repeatN a n)
-
-def visitedLine : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat × Char) → List (Nat × Nat × Dir)
-  | mxmy, x, y, Dir.Up, xs => let pend := (findEnd mxmy x y Dir.Up xs).snd ; List.zip (repeatN x (y - pend + 1)) (List.zip (rangeBetween pend y).reverse (repeatN Dir.Up (y - pend + 1)))
-  | mxmy, x, y, Dir.Down, xs => let pend := (findEnd mxmy x y Dir.Down xs).snd ; List.zip (repeatN x (pend - y + 1)) (List.zip (rangeBetween y pend) (repeatN Dir.Down (pend - y + 1)))
-  | mxmy, x, y, Dir.Right, xs => let pend := (findEnd mxmy x y Dir.Right xs).fst ; List.zip (rangeBetween x pend) (List.zip (repeatN y (pend - x + 1)) (repeatN Dir.Right (pend - x + 1))) 
-  | mxmy, x, y, Dir.Left, xs => let pend := (findEnd mxmy x y Dir.Left xs).fst ; List.zip (rangeBetween pend x).reverse (List.zip (repeatN y (x - pend + 1)) (repeatN Dir.Left (x - pend + 1)))
+def findEnd : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat) → ((Nat × Nat) × Bool)
+  | (_, _), x, y, Dir.Up, xs => let pend := findFirst (λ p ↦ p.snd < y) (findXCol x xs).reverse (x, 10000) ; if pend.snd = 10000 then ((pend.fst, 0), true) else ((pend.fst, pend.snd + 1), false)
+  | (_, maxy), x, y, Dir.Down, xs => let pend := findFirst (λ p ↦ p.snd > y) (findXCol x xs) (x, 10000) ; if pend.snd = 10000 then ((pend.fst, maxy-1), true) else ((pend.fst, pend.snd - 1), false)
+  | (_, _), x, y, Dir.Left, xs => let pend := findFirst (λ p ↦ p.fst < x) (findYRow y xs).reverse (10000, y); if pend.fst = 10000 then ((0, pend.snd), true) else ((pend.fst + 1, pend.snd), false)
+  | (maxx, _), x, y, Dir.Right, xs => let pend := findFirst (λ p ↦ p.fst > x) (findYRow y xs) (10000, y) ; if pend.fst = 10000 then ((maxx-1, pend.snd), true) else ((pend.fst - 1, pend.snd), false)
 
 def matchOnLast : (Nat × Nat) → (Nat × Nat) → Bool
   | (maxx, maxy), (x, y) => Bool.or (Bool.or (x = maxx-1) (y = maxy-1)) (Bool.or (x = 0) (y = 0))
@@ -90,74 +66,115 @@ def nextDir : Dir → Dir
   | Dir.Down => Dir.Left
   | Dir.Left => Dir.Up
 
-def isLoop : List (Nat × Nat × Dir) → Bool
-  | [] => False
-  | (x :: xs) => if xs.contains x then true else isLoop xs 
-
-def weird : (Nat × Nat) × Dir → Nat × Nat × Dir
-  | ((x, y), z) => (x, y, z)
-
-def recurDo : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat × Char) → Nat → List (Nat × Nat × Dir) → List (Nat × Nat × Dir)
-  | _, _, _, _, _, 0, prev => prev
-  | mxmy, x, y, d, xs, Nat.succ n, prev => let line := visitedLine mxmy x y d xs ; if (matchOnLast mxmy (stripLast line.getLast!)) then (List.map weird (List.zip (List.map stripLast line) (repeatN d line.length))) else if (prev.contains line.getLast!) then  (List.map weird (List.zip (List.map stripLast line) (repeatN d line.length))) else line ++ (recurDo mxmy line.getLast!.fst line.getLast!.snd.fst (nextDir d) xs n (line.getLast! :: prev))
-
-def recurDo'' : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat × Char) → Nat → List (Nat × Nat × Dir) → Bool
-  | _, _, _, _, _, 0, prev => isLoop prev
-  | mxmy, x, y, d, xs, Nat.succ n, prev => let line := visitedLine mxmy x y d xs ; if (matchOnLast mxmy (stripLast line.getLast!)) then false else if (prev.contains line.getLast!) then true else (recurDo'' mxmy line.getLast!.fst line.getLast!.snd.fst (nextDir d) xs n (line.getLast! :: prev))
-
-def recurDo' : Nat × Nat → Nat → Nat → Dir → List (Nat × Nat × Char) → Nat → List (Nat × Nat × Dir)
-  | _, _, _, _, _, 0 => []
-  | mxmy, x, y, d, xs, Nat.succ n => let line := visitedLine mxmy x y d xs ; if (matchOnLast mxmy (stripLast line.getLast!)) then (List.map weird (List.zip (List.map stripLast line) (repeatN d line.length))) else line ++ (recurDo' mxmy line.getLast!.fst line.getLast!.snd.fst (nextDir d) xs n)
-
 def gridDims : Nat × Nat := ((grid $ lines data).length, (grid $ lines data).head!.length)
 
-def walk' (gxy : Nat × Nat) (obstacl : List (Nat × Nat × Char))  : List (Nat × Nat × Dir) := recurDo gridDims gxy.fst gxy.snd Dir.Up obstacl 1000 []
+def guardStart : Nat × Nat × Dir := let xy := stripLast $ guardPos $ grid $ lines data; (xy.fst, xy.snd, Dir.Up)
 
-def cornerWalk (gxy : Nat × Nat) (obstacl : List (Nat × Nat × Char)) : Bool := recurDo'' gridDims gxy.fst gxy.snd Dir.Up obstacl 1000 []
+def obst : List (Nat × Nat) := List.map stripLast (obstacles $ grid $ lines $ data)
 
-def walk (gxy : Nat × Nat) (obstacl : List (Nat × Nat × Char))  : List (Nat × Nat × Dir) := recurDo' gridDims gxy.fst gxy.snd Dir.Up obstacl 1000
+def nextCorner : Nat × Nat → Nat × Nat × Dir → List (Nat × Nat × Dir) → List (Nat × Nat) → Nat → List (Nat × Nat × Dir)
+  | _, _, prev, _, 0 => prev
+  | mxmy, (x, y, d), prev, obs, Nat.succ n => let cend := (findEnd mxmy x y d obs); if cend.snd then (prev ++ [(cend.fst.fst, cend.fst.snd, d)]) else if prev.contains (cend.fst.fst, cend.fst.snd, d) then (prev ++ [(cend.fst.fst, cend.fst.snd, d)]) else nextCorner mxmy (cend.fst.fst, cend.fst.snd, nextDir d) (prev ++ [(cend.fst.fst, cend.fst.snd, d)]) obs n
+ 
+def repeatN {α : Type} : α → Nat → List α
+  | _, 0 => []
+  | a, Nat.succ n => a :: (repeatN a n)
+
+-- inclusive
+def range : Nat → Nat → List Nat
+  | x, y => List.map (.+x) (List.range (y - x + 1))
+
+def lineFind : Nat × Nat × Dir → Nat × Nat × Dir → List (Nat × Nat × Dir)
+  | (x1, y1, _), (_, y2, Dir.Up) => (List.zip (repeatN x1 (y1-y2 + 1)) (List.zip ((range y2 y1).reverse) (repeatN Dir.Up (y1-y2 + 1))))
+  | (x1, y1, _), (_, y2, Dir.Down) => (List.zip (repeatN x1 (y2-y1 + 1)) (List.zip (range y1 y2) (repeatN Dir.Down (y2-y1 + 1))))
+  | (x1, y1, _), (x2, _, Dir.Right) => (List.zip (range x1 x2) (List.zip (repeatN y1 (x2-x1 + 1)) (repeatN Dir.Right (x2-x1 + 1))))
+  | (x1, y1, _), (x2, _, Dir.Left) => (List.zip ((range x2 x1).reverse) (List.zip (repeatN y1 (x1-x2 + 1)) (repeatN Dir.Left (x1-x2 + 1))))
+
+def boolCorner : Nat × Nat → Nat × Nat × Dir → List (Nat × Nat × Dir) → List (Nat × Nat) → Nat → Bool × Bool
+  | _, _, _, _, 0 => (false, false)
+  | mxmy, (x, y, d), prev, obs, Nat.succ n => let cend := (findEnd mxmy x y d obs); if cend.snd then (false, true) else if prev.contains (cend.fst.fst, cend.fst.snd, d) then (true, true) else boolCorner mxmy (cend.fst.fst, cend.fst.snd, nextDir d) (prev ++ [(cend.fst.fst, cend.fst.snd, d)]) obs n
+
+def cornersToSquares : List (Nat × Nat × Dir) → List (Nat × Nat × Dir)
+  | [] => []
+  | (_ :: []) => []
+  | (xy1 :: (xy2 :: xs)) => lineFind xy1 xy2 ++ cornersToSquares (xy2 :: xs)
 
 def detBet : Nat × Nat → Nat × Nat → Nat × Nat → Bool
-  | (x, y), (x1, y1), (x2, y2) => if y1 = y2 then Bool.and (x1 ≤ x) (x ≤ x2) else if (y = y1) then (x1 ≤ x) else if (y = y2) then (x ≤ x2) else (Bool.and (y1 ≤ y) (y ≤ y2))
+  | (x, y), (x1, y1), (x2, y2) => if y1 = y2 then Bool.and (x1 ≤ x) (x ≤ x2) else if (y = y1) then (x1 ≤ x) else if (y = y2) then (x ≤ x2) else (Bool.and (y1 < y) (y < y2))
 
-def insertObst : Nat × Nat × Char → List (Nat × Nat × Char) → List (Nat × Nat × Char)
-  | (x, y, c), [] => [(x, y, c)]
-  | (x, y, c), ((x1, y1, _) :: []) => if Bool.and (x1 ≤ x) (y1 ≤ y) then [(x1, y1, c), (x, y, c)] else [(x1, y1, c), (x, y, c)]
-  | (x, y, c), ((x1, y1, _) :: ((x2, y2, _) :: xs)) => if detBet (x, y) (x1, y1) (x2, y2) then (x1, y1, c) :: ((x, y, c) :: ((x2, y2, c) :: xs)) else (x1, y1, c) :: (insertObst (x, y, c) ((x2, y2, c) :: xs))
+def insertObst : Nat × Nat → List (Nat × Nat) → List (Nat × Nat)
+  | (x, y), [] => [(x, y)]
+  | (x, y), ((x1, y1) :: xs) => if (y < y1) then (x, y) :: ((x1, y1) :: xs) else if Bool.and (y == y1) (x < x1) then (x, y) :: ((x1, y1) :: xs) else (x1, y1) :: (insertObst (x, y) xs)
 
-def newBoards : List (Nat × Nat × Char) → List (Nat × Nat × Dir) → List (List (Nat × Nat × Char))
-  | _, [] => []
-  | obs, (x :: xs) => (insertObst (x.fst, x.snd.fst, '#') obs) :: (newBoards obs xs)
+def newBoard : List (Nat × Nat) → (Nat × Nat × Dir) → List (Nat × Nat)
+  | obs, x => insertObst (x.fst, x.snd.fst) obs
 
-def countTrue : List Bool → Nat
-  | [] => 0
-  | (true :: xs) => 1 + countTrue xs
-  | (false :: xs) => countTrue xs
+def walk : List (Nat × Nat × Dir) := (nextCorner gridDims guardStart [guardStart] obst 500)
+
+def loopWalk (obstac : List (Nat × Nat)) (start : (Nat × Nat × Dir)) (prev : List (Nat × Nat × Dir)) (count : Nat) : Bool × Bool := boolCorner gridDims start prev obstac count
 
 def nearEqual : Nat × Nat × Dir → Nat × Nat × Dir → Bool
   | (x, y, _), (x1, y1, _) => Bool.and (x == x1) (y == y1)
 
+theorem dropWhileL (x : Nat × Nat × Dir) (xs : List (Nat × Nat × Dir)) : sizeOf (List.dropWhile (nearEqual x .) xs) < 1 + sizeOf xs := by
+  match xs with
+  | [] => simp[Nat.add_comm 1 (sizeOf x)]
+  | (y :: ys) => simp[List.dropWhile]
+                 match nearEqual x y with
+                 | true => simp[Nat.add_comm 1 (sizeOf y)]
+                           simp[Nat.add_assoc (sizeOf y) 1 (sizeOf ys)]
+                           simp[←Nat.add_assoc 1 (sizeOf y) (1 + sizeOf ys)]
+                           simp[Nat.lt_add_left (1 + sizeOf y) (dropWhileL x ys)]
+                 | false => simp[Nat.add_comm 1 (sizeOf x)]
+
+theorem dropWhileL' (x : Nat × Nat × Dir) (xs : List (Nat × Nat × Dir)) : sizeOf (List.dropWhile (fun y => decide (y.fst = x.fst)) xs) < 1 + sizeOf xs := by
+  match xs with
+  | [] => simp[Nat.add_comm 1 (sizeOf x)]
+  | (y :: ys) => simp[List.dropWhile]
+                 match decide (y.fst = x.fst) with
+                 | true => simp[Nat.add_comm 1 (sizeOf y)]
+                           simp[Nat.add_assoc (sizeOf y) 1 (sizeOf ys)]
+                           simp[←Nat.add_assoc 1 (sizeOf y) (1 + sizeOf ys)]
+                           simp[Nat.lt_add_left (1 + sizeOf y) (dropWhileL' x ys)]
+                 | false => simp[Nat.add_comm 1 (sizeOf x)]
+
+
 def uniqueLine : List (Nat × Nat × Dir) → List (Nat × Nat × Dir)
   | [] => []
   | (x :: xs) => x :: uniqueLine (List.dropWhile (nearEqual x .) xs)
+decreasing_by
+  simp_wf
+  simp[Nat.add_comm 1 (sizeOf x)]
+  simp[Nat.add_assoc]
+  simp[Nat.lt_add_left (sizeOf x) (dropWhileL x xs)]
 
 def splitInto : List (Nat × Nat × Dir) → List (List (Nat × Nat × Dir))
   | [] => []
-  | (x :: xs) => (x :: List.takeWhile (λ y ↦ y.fst=x.fst) xs) :: (splitInto (List.dropWhile (λ y ↦ y.fst=x.fst) xs))
+  | (x :: xs) => (x :: List.takeWhile (λ y ↦ y.fst == x.fst) xs) :: (splitInto (List.dropWhile (λ y ↦ y.fst=x.fst) xs))
+decreasing_by
+  simp_wf
+  simp[Nat.add_comm 1 (sizeOf x)]
+  simp[Nat.add_assoc]
+  simp[Nat.lt_add_left (sizeOf x) (dropWhileL' x xs)]
 
 def prodOrder : Nat × Nat × Dir → Nat × Nat × Dir → Bool
   | (_, y, _), (_, b, _) => y < b
 
 def uniqueWalk (xs : List (Nat × Nat × Dir)) : List (Nat × Nat × Dir) := flatten (List.map (λ y ↦ uniqueLine (Array.qsort (y.toArray) prodOrder).toList) (splitInto (Array.qsort xs.toArray (λ xy ↦ (λ ab ↦ xy.fst < ab.fst))).toList))
 
-def guardPosI : Nat × Nat := (stripLast (guardPos $ grid $ lines data))
+def zipFunc {α β : Type} : (α → β) → List α → List (α × β)
+  | _, [] => []
+  | f, (a :: as) => (a, f a) :: zipFunc f as
 
--- Steps xyd, mxmy, obs
-def goesToEdge : Nat → (Nat × Nat × Dir) → Nat × Nat → List (Nat × Nat × Char) → Bool
-  | 0, _, _, _ => false
-  | n, (x, y, d), mxmy, obs => recurDo'' mxmy x y d obs n []
+def zipTripFunc {α β δ : Type} : (α → β) → (α → δ) → List α → List (α × β × δ)
+  | _, _, [] => []
+  | f, g, (a :: as) => (a, f a, g a) :: zipTripFunc f g as
 
-def cutCases : List (Nat × Nat × Dir) := let points := (walk guardPosI (obstacles $ grid $ lines data)) ; (List.map (λ x ↦ x.snd) (List.filter (λ y ↦ Bool.not $ goesToEdge 500 (y.fst) gridDims (insertObst (y.snd.fst, y.snd.snd.fst, '#') (obstacles $ grid $ lines data))) (List.zip points points.tail!)))
+def walkUpTo (xy : (Nat × Nat × Dir)) (path : List (Nat × Nat × Dir)) : List (Nat × Nat × Dir) := List.takeWhile (λ y ↦ Bool.not (stripLast xy == stripLast y)) path
 
--- Critical (3, 6), (6, 7), (7, 7), (1, 8), (3, 8), (7, 9)
--- #eval! (List.filter (λ y ↦ cornerWalk guardPosI y) (newBoards (obstacles $ grid $ lines data) (uniqueWalk (walk guardPosI (obstacles $ grid $ lines data))))).length
+def findPrev : Nat × Nat × Dir → List (Nat × Nat × Dir) → Nat × Nat × Dir
+  | x, [] => x -- Should never happen
+  | x, (_ :: []) => x -- Similarly should never happen
+  | x, (x1 :: (y :: xs)) => if (stripLast x == stripLast y) then x1 else findPrev x (y :: xs)
+
+def answer : Nat := let corners := cornersToSquares walk.tail!; (List.filter (λ obs ↦ (loopWalk obs guardStart [guardStart] 1000).fst) (List.map (newBoard obst .) (uniqueWalk corners))).length
--- 2024/day6/LeanSoln/Main.lean
+++ 2024/day6/LeanSoln/Main.lean
@@ -1,4 +1,4 @@
-import LeanSoln
+import LeanSoln.soln2
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println s!"{answer}"
