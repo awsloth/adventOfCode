7cd00635345c0e98af2cb11fab85a70544175118
Anna <awsloth@gmail.com>
2025-12-03 09:34:58 +0000

    Clean up code

--- 2025/day3/solution.hs
+++ 2025/day3/solution.hs
@@ -1,32 +1,22 @@
-import Data.Char
-import Data.List
+import Data.Char (digitToInt)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 
 main :: IO Int
 main = do
        x <- readFile "input.txt"
-       pure (part2 $ lines x)
+       pure (part1 $ lines x)
 
-dropLast :: [a] -> [a]
-dropLast [x] = []
-dropLast (x : xs) = x : dropLast xs
+findMaxGen :: Int -> [Int] -> Int
+findMaxGen 1 xs = maximum xs
+findMaxGen x xs = 10^(x-1)*s + findMaxGen (x - 1) (drop (fromJust (elemIndex s xs) + 1) xs)
+ where
+  s :: Int
+  s = maximum (take (length xs - x + 1) xs)
 
 part1 :: [String] -> Int
-part1 = foldr ((+) . findMax . map digitToInt) 0
-
-findMax :: [Int] -> Int
-findMax xs = let s = maximum (dropLast xs)
-              in
-               case elemIndex s xs of
-                Nothing -> undefined
-                (Just i) -> 10*s + maximum (drop (i + 1) xs)
+part1 = foldr ((+) . findMaxGen 2 . map digitToInt) 0
 
 part2 :: [String] -> Int
 part2 = foldr ((+) . findMaxGen 12 . map digitToInt) 0
 
-findMaxGen :: Int -> [Int] -> Int
-findMaxGen 1 xs = maximum xs
-findMaxGen x xs = let s = maximum (take (length xs - x + 1) xs)
-                   in
-                    case elemIndex s xs of
-                     Nothing -> undefined
-                     (Just i) -> (10^(x-1)*s) + findMaxGen (x - 1) (drop (i + 1) xs)
