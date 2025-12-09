06ec978565a5ef30b765032efebe215eb1c44b2a
Anna <awsloth@gmail.com>
2025-12-09 23:44:07 +0000

    Clean up and fix

--- 2025/day9/solution.hs
+++ 2025/day9/solution.hs
@@ -5,7 +5,7 @@ import Data.List
 
 main :: IO ()
 main = do
-       x <- readFile "input.txt"
+       x <- readFile "test.txt"
        print $ part2 x
 
 type Point = (Int, Int)
@@ -28,54 +28,44 @@ part2 s = maximum . map area . filter (inGreen points) $ allPairs points
  where
   points = findRed s
 
-inGreen :: [Point] -> (Point, Point) -> Bool
-inGreen xs (a, b) | null inside && null onedge = noLinesThrough outside
-                  | null inside = all notthrough onedge && noLinesThrough outside
-                  | otherwise = False
+inside :: Point -> Point -> Point -> Bool
+inside (a, b) (c, d) (x, y) = x < max a c && x > min a x && y < max b d && y > min b d
+
+onleft   (a, b) (c, d) (x, y) = x == min a c && min b d <= y && y <= max b d
+onright  (a, b) (c, d) (x, y) = x == max a c && min b d <= y && y <= max b d
+onbottom (a, b) (c, d) (x, y) = y == max b d && min a c <= x && x <= max a c
+ontop    (a, b) (c, d) (x, y) = y == min b d && min a c <= x && x <= max a c
+corner   (a, b) (c, d) (x, y) = (x == a || x == c) && (y == b || y == d)
+
+edge :: Point -> Point -> [Point] -> [(Point, Point, Point)]
+edge a b xs = [(x, y, z) | (x, (y, z)) <- zip xs (zip (tail xs ++ [head xs]) (tail (tail xs) ++ [head xs, head (tail xs)])), y /= a, y /= b, ontop a b y || onbottom a b y || onleft a b y || onright a b y]
+
+noLinesThrough :: (Point, Point) -> [Point] -> Bool
+noLinesThrough (a, b) xs = null [(x, y) | (x, y) <- zip xs (tail xs ++ [head xs]), (verticalLine (x, y) && (fst x == fst y)) || (horizontalLine (x, y) && (snd x == snd y))]
  where
-  maxx = max (fst a) (fst b)
-  minx = min (fst a) (fst b)
+  verticalLine (x, y) = min (snd x) (snd y) < min (snd a) (snd b)
+                      && max (snd x) (snd y) > max (snd a) (snd b)
+  horizontalLine (x, y) = min (fst x) (fst y) < min (fst a) (fst b)
+                      && max (fst x) (fst y) > max (fst a) (fst b)
 
-  maxy = max (snd a) (snd b)
-  miny = min (snd a) (snd b)
-
-  shiftround = tail (tail xs) ++ [head xs, head (tail xs)]
-
-  inside = [1 | (x, y) <- xs, x < maxx, minx < x, y < maxy, miny < y]
-  outside = [(x,y) | (x, y) <- xs, x > maxx || minx > x || y > maxy || miny > y]
-  onedge = [(x, y, z) | (x, (y, z)) <- zip xs (zip (tail xs ++ [head xs]) shiftround), y /= a, y /= b, ontop y || onbottom y || onleft y || onright y]
-
-  onleft    (a, b) = a == minx && miny <= b && b <= maxy
-  onright (a, b) = a == maxx && miny <= b && b <= maxy
-  onbottom (a, b) = b == maxy && minx <= a && a <= maxx
-  ontop (a, b) = b == miny && minx <= a && a <= maxx
-  corner (a, b) =  a == minx && (b == miny || b == maxy)
-                || a == maxx && (b == miny || b == maxy)
-
-
-  notthrough ((v, w), e, (x, y)) | corner e   = validCorner e
-                                 | ontop e    = y <= snd e && w <= snd e
-                                 | onbottom e = y >= snd e && w >= snd e
-                                 | onright e  = x >= fst e && v >= fst e
-                                 | onleft e   = x <= fst e && v <= fst e
+inGreen :: [Point] -> (Point, Point) -> Bool
+inGreen xs (a, b) = not (any (inside a b) xs)
+                  && all notthrough (edge a b xs)
+                  && noLinesThrough (a, b) xs
+ where
+  notthrough ((v, w), e, (x, y)) | corner a b e   = validCorner e
+                                 | ontop a b e    = y <= snd e && w <= snd e
+                                 | onbottom a b e = y >= snd e && w >= snd e
+                                 | onright a b e  = x >= fst e && v >= fst e
+                                 | onleft a b e   = x <= fst e && v <= fst e
  
   validCorner :: Point -> Bool
-  validCorner e | fromJust (elemIndex e xs) < fromJust (elemIndex b xs) && fromJust (elemIndex e xs) > fromJust (elemIndex a xs) = validHelper' e
-                | otherwise = validHelper e
+  validCorner e | fromJust (elemIndex e xs) < fromJust (elemIndex b xs)
+               && fromJust (elemIndex e xs) > fromJust (elemIndex a xs) = validHelper b a e
+                | otherwise = validHelper a b e
                
-  
-  validHelper :: Point -> Bool
-  validHelper e | fst a <= fst b && snd a <= snd b = fst e == fst a
-                | fst a <= fst b && snd a >= snd b = fst e == fst b
-                | fst a >= fst b && snd a <= snd b = fst e == fst b
-                | fst a >= fst b && snd a >= snd b = fst e == fst a
-
-  validHelper' :: Point -> Bool
-  validHelper' e | fst a <= fst b && snd a <= snd b = fst e == fst b
-                 | fst a <= fst b && snd a >= snd b = fst e == fst a
-                 | fst a >= fst b && snd a <= snd b = fst e == fst a
-                 | fst a >= fst b && snd a >= snd b = fst e == fst b
-
-  verts = [fst x | x <- outside, y <- outside, fst x == fst y, x /= y, minx <= fst x, fst x <= maxx, min (snd x) (snd y) <= miny, maxy <= max (snd x) (snd y)]
-  hors  = [snd y | x <- outside, y <- outside, snd x == snd y, x /= y, miny <= snd x, snd x <= maxy, min (fst x) (fst y) <= minx, maxx <= max (fst x) (fst y)]
-  noLinesThrough xs = null (verts ++ hors)
+  validHelper :: Point -> Point -> Point -> Bool
+  validHelper a b e | fst a <= fst b && snd a <= snd b = fst e == fst a
+                    | fst a <= fst b && snd a >= snd b = fst e == fst b
+                    | fst a >= fst b && snd a <= snd b = fst e == fst b
+                    | fst a >= fst b && snd a >= snd b = fst e == fst a
